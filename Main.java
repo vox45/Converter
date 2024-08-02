12e489228041d41f5e8458a7d8250be43b5ff175
vox45 <golovkodave19@gmail.com>
2024-08-02 20:40:28 +0100

    Added kilometers to miles conversion

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is my project");
+        double miles = 10;
+        double kilometers = milesToKilometers(miles);
+        System.out.println(miles + " miles is equal to " + kilometers + " kilometers");
+
+        double km = 16.0934;
+        double mi = kilometersToMiles(km);
+        System.out.println(km + " kilometers is equal to " + mi + " miles");
+    }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 }
