e8708838b54e069e24d28d006b500edc35052052
vox45 <golovkodave19@gmail.com>
2024-08-18 16:27:26 +0300

    Initial commit

--- Main.java
+++ Main.java
@@ -1,20 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is my project");
-        double miles = 10;
-        double kilometers = milesToKilometers(miles);
-        System.out.println(miles + " miles is equal to " + kilometers + " kilometers");
-
-        double km = 16.0934;
-        double mi = kilometersToMiles(km);
-        System.out.println(km + " kilometers is equal to " + mi + " miles");
-    }
-
-    public static double milesToKilometers(double miles) {
-        return miles * 1.60934;
-    }
-
-    public static double kilometersToMiles(double kilometers) {
-        return kilometers / 1.60934;
+        System.out.println("Проект Converter");
+        System.out.println("Версія 1.0");
     }
 }
